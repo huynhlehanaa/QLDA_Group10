96b286b7d7a18c85553e3e6d4142afa0bb9dd17d
huynhlehana <huynhlehana@gmail.com>
2026-04-21 23:59:33 +0700

    commit

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.user import User, LoginLog
+from app.models.organization import Organization, Department
+from app.models.notification import Notification
+
+__all__ = [
+    "User", "LoginLog", "Organization", "Department", "Notification",
+]
